5e3bd8345d8522c9d9ec927dfa566708eb98c158
Mustafa Ersin Şişman <musisman@k1m30s04.42istanbul.com.tr>
2024-11-12 17:21:19 +0300

    iskelet oluştu

--- ft_printf.c
+++ ft_printf.c
@@ -11,13 +11,53 @@
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include <stdarg.h>
+
+static void	cspdiuxX(char c)
+{
+	if (c == 'c')
+		ft_putchar();
+	else if (c == 's')
+		ft_putstr();
+	else if (c == 'p')
+
+	else if (c == 'd')
+		ft_putnbr();
+	else if (c == 'i')
+		ft_putnbr();
+	else if (c == 'u')
+		ft_putnbr();
+	else if (c == 'x')
+
+	else if (c == 'X')
+
+	else if (c == '%')
+		ft_putchar('%');
+	else
+
+}	
 
 int	ft_printf(const char *s, ...)
 {
 	va_list	args;
+	size_t	i;
 
-	va_start(args,*s);
-	va_arg(args,*s);
+	i = 0;
+	va_start(args, s[i]);
+	while (s[i])
+	{
+		if (s[i] == '%')
+		{
+			while (s[i + 1] && s[i + 1] == ' ')
+				i++;
+			cspdiuxX(s[i + 1]);
+			i++;
+		}
+		else
+		{
+			ft_putchar(s[i]);
+			i++;
+		}
+	}
+	va_arg(args, s[i]);
 	va_end(args);
 }
